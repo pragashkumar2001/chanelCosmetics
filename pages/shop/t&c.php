2dfb1add2fde8d2239560b43fa8df18b9e715698
PragashKumar Srikanthan <ranger.pragash@gmail.com>
2023-10-06 05:35:02 +0530

    t&c , About , faq

--- pages/shop/t&c.php
+++ pages/shop/t&c.php
@@ -20,31 +20,43 @@ include("./shared/head.php");
     <br>
 
     <ul>
-      <li>These are the terms and conditions (which included our copyright notice) on which we do business for goods ordered through this website. These terms and conditions are designed to set out clearly our responsibilities and your rights. Please read these terms and conditions carefully before placing your order and retain a copy for future reference.</li>
-      <li>We reserve the right to vary these terms and conditions at any time but, in respect of any ordered goods, the terms and conditions which apply shall be those which you accepted when you placed your order. Otherwise, no alteration of these terms and conditions shall apply unless agreed in writing between us and you.</li>
-      <li>A contract is formed between us and you when (and not before) we have sent you confirmation by email to the email address you have given and receive settlement of your credit/debit card payment. If you want to cancel or amend your order you must contact us as soon as possible. A repocessing fee will be charge for all Cancel and Amends to Made to Order Items.</li>
-      <li>Every effort will be made to ensure that prices shown in our website are accurate at the time you place your order. If an error is found prior to dispatch of the goods, we will inform you as soon as possible and offer you the option of reconfirming your order at the correct price or canceling your order. If you cancel, we will refund or re-credit you for any sum that has been paid by you or debited from your credit card for the goods.</li>
-      <li>Pictures, illustrations or descriptions or any other information submitted or contained in this website or other advertising matter are for general information and guidance. There may be minor variations between the goods as shown or described on our website and those dispatched to you without affecting their function, quality or price.</li>
-      <li>Payment for goods ordered can be made by any method shown in this website at the time you place your order. All payments are taken in Srilankan Rupees and all purchases are inclusive of VAT at the prevailing SL rate. Our VAT Registration number is SL308024929.</li>
-      <li>The ordered goods will be delivered to the address entered by you on the on-line order. If there is no one at the address given who is competent to accept delivery of the goods, you will be notified of an alternative delivery date or a place to collect the goods. Ordered goods will normally be delivered within 3 - 5 weeks of your order being accepted unless otherwise notified to you by us. If we are unable to deliver the goods within 10 weeks of your order, we will inform you as soon as possible and you will be entitled to cancel the order and obtain a refund or re-credit for any sum that has been paid by you or debited from your credit card for the goods. We will not be liable for any loss or damage suffered by you through any reasonable or unavoidable delay in delivery.</li>
-      <li>All orders are accepted subject to availability of the goods. If any ordered goods are not readily available to us, we will inform you as soon as possible, and refund or re-credit in full any sum that has been paid by you or debited from your credit card.</li>
 
+      <h3>Acceptance of Terms</h3>
+      <li>By accessing and using the Chanel Cosmetics website, you agree to comply with and be bound by these Terms and Conditions. If you do not agree to these terms, please refrain from using our Website.</li>
 
+      <h3>Privacy Policy</h3>
+      <li>Your use of our Website is also governed by our Privacy Policy, which outlines how we collect, use, and protect your personal information. Please review our Privacy Policy to understand our practices.</li>
 
-      <h3>Copyright Notice</h3>
+      <h3>Product Information</h3>
+      <li>Chanel Cosmetics strives to provide accurate product information, including descriptions, prices, and availability. However, we do not warrant the accuracy or completeness of this information and reserve the right to modify or update it without notice.</li>
+   
+      <h3>Orders and Payment</h3>
+      <li>To place an order, you must provide accurate and current information, including billing and shipping details.</li>
+      <li>Chanel Cosmetics accepts payment via the methods specified on our Website. Payment information is subject to verification, and orders may be canceled or delayed if issues arise.</li>
+      <li>Pricing errors: In the event of a pricing error, we reserve the right to cancel any orders placed at the incorrect price and offer a refund or an opportunity to re-order at the corrected price.</li>
+      
+      <h3>Shipping and Delivery</h3>
+      <li>We aim to process and ship orders promptly, but shipping times may vary based on location and product availability.</li>
+      <li>Chanel Cosmetics is not responsible for any delays or damages caused by shipping carriers. Please refer to our Shipping Policy for more details.</li>
 
+      <h3>Returns and Refunds</h3>
+      <li>Our Returns & Refunds Policy outlines the process for returning products and requesting refunds. By placing an order, you agree to these policies.</li>
+      <li>We reserve the right to refuse returns or refunds for products that do not meet our criteria.</li>
 
-      <li>It is a condition of Fahsion Hub allowing you access to the material on this website that you accept the terms and conditions of this notice. The contents of this website are protected by copyright. The copying or incorporation into any other work of part or all of the material available on this website in any form is prohibited except that you may print or download extracts of the materials on this site for your personal use. Fashion Hub is a trademark in the Sri Lanka and other countries. The trademark may not be used in connection with any product or service that is not supplied by Fashion Hub.</li>
-      <li>Links or redirections to other websites or references to products and services offered by third parties are provided for your convenience only. They do not constitute an endorsement or approval in any form by us. We have no control or responsibility for the companies that operate the sites the content they may provide the privacy policies and terms of use they may adopt or the products and services they offer. We neither assume nor accept any liability for your use of third party or linked web sites and your use of such is entirely at your own risk. It is up to you to take precautions to ensure that whatever you for your use is free of such items as viruses, worms, Trojan horses and other items of a destructive nature.</li>
+      <h3>Intellectual Property</h3>
+      <li>The content on this Website, including text, images, logos, and trademarks, is the intellectual property of Chanel Cosmetics and is protected by copyright and trademark laws.</li>
+      <Li>You may not use our content for commercial purposes without our express written consent.</Li>
 
-      <h3>Sample Dresses</h3>
-      <li>We provide the sample dress service exclusively in the Sri Lanka.</li>
-      <li>Dresses must be returned within 28 days of dispatch. Failure to do so will result in the full cost of the dress and top to be charged to you. If you need to extend the return date please contact us as soon as possible. If we have not heard from you we will proceed with the charge.</li>
-      <li>Second Class return postage is provided within the Sri Lanka only. If you choose your own service, please use a tracked service as we cannot be responsible for lost items.</li>
-      <li>NO REFUND will be given for this service. Upon the safe return of your sample dress we will issue a store credit on your account for the cost of the service excluding postage.</li>
+      <h3>Termination</h3>
+      <li>Chanel Cosmetics reserves the right to terminate or suspend your access to the Website without notice for any violation of these Terms and Conditions.</li>
+      
+      <h3>Changes to Terms and Conditions</h3>
+      <li>Chanel Cosmetics may update these Terms and Conditions at any time. It is your responsibility to review them periodically for changes.</li>
 
       <h5>
-        Please note that all Made to Order items carry a return reprocessing fee. By placing your order with us for any Made to Order Item you agree to our Returns and Refund Policy.
+      By using the Chanel Cosmetics website, you acknowledge that you have read, understood, and agree to these Terms and Conditions.
+      <br>
+      Last updated: 25/09/2023
       </h5>
 
     </ul>
