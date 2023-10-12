a55b1b8177000465979c75cbe3a28820ff76ed43
AmruNizwar <amrunizwar760@gmail.com>
2023-10-12 23:46:29 +0530

    login signup changes

--- pages/auth/signin.php
+++ pages/auth/signin.php
@@ -6,35 +6,45 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Fashion Hub - SignIn</title>
-  <link rel="stylesheet" href="../../assets/css/main.css?v=<?php echo time(); ?>" />
-  <link rel="stylesheet" href="../../assets/css/auth.css?v=<?php echo time(); ?>" />
+<link rel="stylesheet" href="../../assets/css/login.css?v=<?php echo time(); ?>" />
 </head>
 
 <body>
-  <main class="signin-form">
-    <h1>Sign In</h1>
-    <form action="authHandler.php" method="post" class="form-content">
 
-      <div class="form-group">
-        <label for="txtEmail">Email</label>
-        <input type="email" name="txtEmail" id="txtEmail" required />
-      </div>
+		<div class="wrapper">
+			<div class="inner">
+				<div class="image-holder">
+					<img src="../../assets/images/SignPic.png" alt="">
+				</div>
+				<form action="authHandler.php" method="post" class="form-content">
+					<h3>Sign in</h3>
+				
+					<div class="form-holder active">
+						<input type="text" placeholder="e-mail" class="form-control" name="txtEmail" id="txtEmail" required>
+					</div>
+                    <div class="form-holder">
+						<input type="password" placeholder="password" class="form-control" style="font-size: 15px;" name="txtPassword" id="txtPassword" required>
+					</div>
+					<div class="form-login">
+						<button name="signInBtn" id="signInBtn">Sign in</button>
+						<p>Dont't Have an account? <a href="signup.php">Register</a></p>
+            <p>OR   <br><br></p>
+            <p><a href="../shop/home.php">   Go back to Home</a></p>
+					</div>
+					
 
-      <div class="form-group">
-        <label for="txtPassword">Password</label>
-        <input type="password" name="txtPassword" id="txtPassword" required />
-      </div>
+					
+				</form>
+			</div>
+		</div>
 
-      <input type="submit" name="signInBtn" id="signInBtn" value="Login" />
-
-      <p class="form-footer">
-        Don't Have an Account? <a href="signup.php">Sign Up</a>
-        <br><br>
-        <a href="../shop/home.php">Go back to Home</a>
-      </p>
-
-    </form>
-  </main>
-</body>
+		<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+		<script>$(function(){
+	$('.form-holder').delegate("input", "focus", function(){
+		$('.form-holder').removeClass("active");
+		$(this).parent().addClass("active");
+	})
+})</script>
+	</body>
 
 </html>
--- pages/auth/signup.php
+++ pages/auth/signup.php
@@ -6,8 +6,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Fashion Hub - Signup</title>
-  <link rel="stylesheet" href="../../assets/css/main.css?v=<?php echo time(); ?>" />
-  <link rel="stylesheet" href="../../assets/css/auth.css?v=<?php echo time(); ?>" />
+
+  <link rel="stylesheet" href="../../assets/css/registration.css?v=<?php echo time(); ?>" />
 </head>
 
 <body>
@@ -24,54 +24,54 @@
       return true;
     }
   </script>
-  <main class="signup-form">
-    <h1>Sign Up</h1>
+
+<div class="wrapper">
+  <div class="inner">
+    <div class="image-holder">
+      <img src="../../assets/images/registrationPic.jpg" alt="">
+    </div>
     <form action="authHandler.php" method="post" onsubmit="return validate();" class="form-content">
-
-      <div class="form-group">
-        <label for="txtFirstName">First Name</label>
-        <input type="text" name="txtFirstName" id="txtFirstName" required />
+      <h3>Sign Up</h3>
+      <div class="form-holder active">
+        <input type="text" name="txtFirstName" id="txtFirstName" placeholder="First Name" class="form-control" required>
       </div>
-
-      <div class="form-group">
-        <label for="txtLastName">Last Name</label>
-        <input type="text" id="txtLastName" name="txtLastName" required />
+      <div class="form-holder">
+        <input type="text" id="txtLastName" name="txtLastName" placeholder="Last Name" class="form-control" required>
       </div>
-
-      <div class="form-group">
-        <label for="txtEmail">Email</label>
-        <input type="email" name="txtEmail" id="txtEmail" required />
+      <div class="form-holder">
+        <input type="text" name="txtEmail" id="txtEmail" placeholder="e-mail" class="form-control" required>
       </div>
-
-      <div class="form-group">
-        <label for="txtPassword">Password</label>
-        <input type="password" name="txtPassword" id="txtPassword" required />
+      <div class="form-holder">
+        <input type="text" name="txtAddress" id="txtAddress" placeholder="Address" class="form-control" required>
       </div>
+      <div class="form-holder">
+        <input type="number" name="txtPhoneNumber" id="txtPhoneNumber" placeholder="07x xxx xxxx" class="form-control" required>
 
-      <div class="form-group">
-        <label for="txtConfirmPassword">Confirm Password</label>
-        <input type="password" name="txtConfirmPassword" id="txtConfirmPassword" required />
       </div>
-
-      <div class="form-group">
-        <label for="txtAddress">Address</label>
-        <textarea name="txtAddress" id="txtAddress" required></textarea>
+      <div class="form-holder">
+        <input type="password" name="txtPassword" id="txtPassword" placeholder="Password" class="form-control" style="font-size: 15px;" required>
       </div>
-
-      <div class="form-group">
-        <label for="txtPhoneNumber">Phone Number</label>
-        <input type="text" name="txtPhoneNumber" id="txtPhoneNumber" required />
+      <div class="form-holder">
+        <input type="password" name="txtConfirmPassword" id="txtConfirmPassword" placeholder="Confirm Password" class="form-control" style="font-size: 15px;" required>
       </div>
-
-      <input type="submit" name="signUpBtn" id="signUpBtn" value="Register" />
-
-      <p class="form-footer">
-        Already Have an Account? <a href="signin.php">Sign In</a>
-        <br><br>
-        <a href="../shop/home.php">Go back to Home</a>
-      </p>
+    
+      <div class="form-login">
+        <button name="signUpBtn" id="signUpBtn">Sign up</button>
+        <p>Already Have account? <a href="signin.php">Login</a></p>
+        <p>OR   <br><br></p>
+        <p><a href="../shop/home.php">   Go back to Home</a></p>
+      </div>
     </form>
-  </main>
+  </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script>$(function(){
+	$('.form-holder').delegate("input", "focus", function(){
+		$('.form-holder').removeClass("active");
+		$(this).parent().addClass("active");
+	})
+})</script>
 </body>
 
 </html>
